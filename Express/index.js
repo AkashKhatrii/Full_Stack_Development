ef5863f994b279a7c048f7007ccf01f7da786aff
Akash Khatri <akashkhatri1001@gmail.com>
2024-04-13 21:27:19 -0600

    Add body-parser and nodemon content

--- Express/index.js
+++ Express/index.js
@@ -1,15 +1,16 @@
 const fs = require("fs");
 const express = require("express")
+const bodyParser = require("body-parser");
 const port = 3000
 
 const app = express();
-
+app.use(bodyParser.json()); // any time body has some json, this handles it.
 app.get('/', function(req, res){
     res.send('Hello World!');
 })
 
 app.post('/conversations', function(req, res){
-    console.log(req.headers);
+    console.log(req.body);
     res.send({
       msg: "2 + 2 = 4" 
     })
