f6e1cfb82fe7798ccfe0f98da31db39af2d76e38
AkashKhatrii <akashkhatri1001@gmail.com>
2024-04-22 19:59:06 -0600

    Implement todo_react

--- todo_react/src/App.jsx
+++ todo_react/src/App.jsx
@@ -2,18 +2,42 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([{
+    title: 'Go to gym',
+    description: 'Go to gym from 3-7',
+    completed: false
+  },
+  {
+    title: 'Stduy DSA',
+    description: 'Stduy DSA from 9-100',
+    completed: true
+  }])
 
+  function addTodo(){
+    setTodos([...todos, {
+      title: 'New Todo',
+      description: 'New desc'
+    }])
+  }
   return (
-      <div>
-        
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
+      <div> 
+          <button onClick={addTodo}>Add Todo</button>
+
+        {todos.map(todo => {
+          return <Todo title={todo.title} description={todo.description}/>
+        })}
       </div>
       
   )
 }
 
-export default App
+function Todo(props){
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.description}</h2>
+    </div>
+  )
+}
+
+export default App;
